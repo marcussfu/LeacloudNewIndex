5faf9f880408b08e703bd40d611155980296f324
marcus.f <marcus.f@leacloud.com>
2021-03-15 16:24:23 +0800

    Adjust i18n

--- assets/js/changeLanguage.js
+++ assets/js/changeLanguage.js
@@ -7,20 +7,34 @@ i18next.init({
       en: {
         translation: {
           "login": "Login",
-          "logup": "Logup"
+          "logup": "Logup",
+          "languageSelect": "Language",
+          "contactUs": "contactUs",
+          "bannerTitle": "bannerTitle",
+          "bannerSubtitle": "bannerSubtitle",
+          "bannerContentText": "bannerContentText",
         }
       },
       tw: {
         translation: {
           "login": "登入",
-          "logup": "註冊"
+          "logup": "註冊",
+          "languageSelect": "語言選擇",
+          "contactUs": "聯繫我們",
+          "bannerTitle": "網路拉近世界距離",
+          "bannerSubtitle": "Leacloud打通你我距離",
+          "bannerContentText": "立即試用",
         }
       },
       sc: {
         translation: {
           "login": "登入",
-          "logup": "註册"
-
+          "logup": "註册",
+          "languageSelect": "语言选择",
+          "contactUs": "联繫我们",
+          "bannerTitle": "网路拉近世界距离",
+          "bannerSubtitle": "Leacloud打通你我距离",
+          "bannerContentText": "立即试用",
         }
       }
     }
@@ -32,6 +46,12 @@ i18next.init({
   function updateContent() {
     document.getElementById('login').innerHTML = i18next.t('login');
     document.getElementById('logup').innerHTML = i18next.t('logup');
+    document.getElementById('languageSelect').innerHTML = i18next.t('languageSelect');
+    document.getElementById('contactUs').innerHTML = i18next.t('contactUs');
+    document.getElementById('bannerTitle').innerHTML = i18next.t('bannerTitle');
+    document.getElementById('bannerSubtitle').innerHTML = i18next.t('bannerSubtitle');
+    document.getElementById('bannerContentText').innerHTML = i18next.t('bannerContentText');
+
   }
 
   function getChangeLang(text) {
